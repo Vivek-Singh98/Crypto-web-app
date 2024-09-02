720086d5b042f73066ae6c34638cf9556e5503b1
vivek-singh98 <vivekit2001@gmail.com>
2024-09-02 17:02:43 +0530

    crypto

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='text-center py-2 border-t-2 border-t-gray-500'>
+            &copy; 2024 Crypto-web-app | Vivek Singh
+            <br />
+            <a href="https://github.com/vivek-singh98" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
+                GitHub: vivek-singh98
+            </a>
+        </div>
+    )
+}
+
+export default Footer
